8127382ae159ab71a35a3e93e2538f1bcc0b6efa
laptop-Jyi-PC\laptop-Jyi <yi_jian27@163.com>
2017-09-18 17:02:17 +0800

    新版SFP的测试版本，用于Golden Eagle。

---
+++ 16F18346_I2C2_25G78125_2017.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=16F18346_I2C2_25G78125_2017.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/16F18346_I2C2_25G78125_2017.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=16f18346i2c225g781252017.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/16f18346i2c225g781252017.x.tar
--- README.md
+++
@@ -1,2 +0,0 @@
-# PIC_TEST
-Test PIC and ACC_100G_QSFP28 or ACC_100G_SFP28 by write register
